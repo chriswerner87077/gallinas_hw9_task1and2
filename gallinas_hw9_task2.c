a7b562345a0a55091e78615d9655ab95c41c3707
Orlando Salas <orlandosalas@mail.weber.edu>
2017-04-06 23:03:54 +0000

    initial commit for task 2

---
+++ gallinas_hw9_task2.c
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  gallinas_hw9_task2.c
+ *       	Usage:  ./gallinas_hw9_task2.c
+ *    Description:  ozone problem
+ *
+ *        Version:  1.0
+ *        Created:  04/06/2017 11:03:04 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Orlando Salas (), orlandosalas@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>
+
+/* Function Prototypes */
+
+/* Main Program */
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
